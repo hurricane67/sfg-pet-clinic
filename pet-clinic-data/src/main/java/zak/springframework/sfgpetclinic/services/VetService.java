02c0b5c189ee8943bd63763a9a75e7a51dacd2dc
hurricane67 <azahaf.zakariae@gmail.com>
2021-01-13 15:39:50 +0100

    Refactor Interface Service to common CRUD base Interface.

--- pet-clinic-data/src/main/java/zak/springframework/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/zak/springframework/sfgpetclinic/services/VetService.java
@@ -2,14 +2,6 @@ package zak.springframework.sfgpetclinic.services;
 
 import zak.springframework.sfgpetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet,Long> {
 
 }
