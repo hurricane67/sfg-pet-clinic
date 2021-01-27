02d3191ef586f05ac614fc4dc6bf8a3710698b58
hurricane67 <azahaf.zakariae@gmail.com>
2021-01-27 16:31:38 +0100

    Added Specialty Map Impl.

---
+++ pet-clinic-data/src/main/java/zak/springframework/sfgpetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package zak.springframework.sfgpetclinic.services;
+
+import zak.springframework.sfgpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty,Long>{
+}
